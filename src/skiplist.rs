13f2db8fdd091c907332ff48dc299cd531f9f5d2
Yang Keao <keao.yang@yahoo.com>
2019-04-06 23:48:09 +0800

    Fix bugs in random_insert

--- src/skiplist.rs
+++ src/skiplist.rs
@@ -9,7 +9,7 @@ pub struct Node<T> {
 }
 
 pub struct SkipList<T: PartialOrd> {
-    head: RwLock<Option<Arc<RwLock<Node<T>>>>>,
+    head: Arc<RwLock<Node<T>>>,
 }
 
 fn random_height() -> usize {
@@ -29,33 +29,39 @@ impl<T: PartialOrd> Iterator for SkipListIter<T> {
         match &now_guard.nexts[0] {
             Some(next) => {
                 self.now = next.clone();
-                return Some(now.clone());
+                return Some(self.now.clone());
             }
             None => None,
         }
     }
 }
 
-impl<T: PartialOrd> SkipList<T> {
+impl<T: PartialOrd + Default> SkipList<T> {
     pub fn new() -> Self {
+        let mut nexts = Vec::new();
+        for _ in 0..MAX_LEVEL {
+            nexts.push(None);
+        }
         return Self {
-            head: RwLock::new(None),
+            head: Arc::new(RwLock::new(Node {
+                value: T::default(),
+                nexts,
+            })),
         };
     }
 
-    pub fn iter(&self) -> Option<SkipListIter<T>> {
-        match &*self.head.read().unwrap() {
-            Some(head) => Some(SkipListIter::<T> { now: head.clone() }),
-            None => None,
+    pub fn iter(&self) -> SkipListIter<T> {
+        SkipListIter::<T> {
+            now: self.head.clone(),
         }
     }
 
     pub fn insert(&self, val: T) {
-//        println!();
-//        unsafe {
-//            let val = &val as *const T as *const u32;
-//            println!("INSERTING {}", *val);
-//        }
+        //        println!();
+        //        unsafe {
+        //            let val = &val as *const T as *const u32;
+        //            println!("INSERTING {}", *val);
+        //        }
 
         let (_now, prev) = self.find_greater_or_equal(&val);
 
@@ -64,10 +70,7 @@ impl<T: PartialOrd> SkipList<T> {
         for _ in 0..MAX_LEVEL {
             nexts.push(None);
         }
-        let new_node = Arc::new(RwLock::new(Node {
-            value: val,
-            nexts,
-        }));
+        let new_node = Arc::new(RwLock::new(Node { value: val, nexts }));
 
         for i in 0..height {
             match &prev[i] {
@@ -78,27 +81,18 @@ impl<T: PartialOrd> SkipList<T> {
                         }
                         None => {}
                     }
-//                    unsafe {
-//                        let val = &prev.read().unwrap().value as *const T as *const u32;
-//                        if i == 0 {
-//                            println!("{} {}", *val, i);
-//                        }
-//                    }
+                    //                    unsafe {
+                    //                        let val = &prev.read().unwrap().value as *const T as *const u32;
+                    //                        if i == 0 {
+                    //                            println!("{} {}", *val, i);
+                    //                        }
+                    //                    }
                     prev.write().unwrap().nexts[i] = Some(new_node.clone());
                 }
                 None => {
-                    // TODO: is this situation special?
+                    unreachable!();
                 }
             }
-        }
-
-        let head_guard = self.head.read().unwrap();
-        match &*head_guard {
-            None => {
-                drop(head_guard);
-                self.head.write().unwrap().replace(new_node);
-            }
-            Some(_) => {}
         }
     }
 
@@ -110,41 +104,36 @@ impl<T: PartialOrd> SkipList<T> {
         Vec<Option<Arc<RwLock<Node<T>>>>>,
     ) {
         let mut prev: Vec<Option<Arc<RwLock<Node<T>>>>> = vec![None; MAX_LEVEL];
-        match &*self.head.read().unwrap() {
-            Some(head) => {
-                let mut x = head.clone();
+        let mut x = self.head.clone();
 
-                let mut level = MAX_LEVEL - 1;
-                loop {
-                    let x_guard = x.read().unwrap();
-                    match &x_guard.nexts[level] {
-                        Some(next) => {
-                            if &next.read().unwrap().value < val {
-                                let new_x = next.clone();
-                                drop(x_guard);
-                                x = new_x;
-                                continue;
-                            } else {
-                                prev[level] = Some(x.clone());
-                                if level == 0 {
-                                    return (Some(next.clone()), prev);
-                                } else {
-                                    level -= 1;
-                                }
-                            }
-                        }
-                        None => {
-                            prev[level] = Some(x.clone());
-                            if level == 0 {
-                                return (None, prev);
-                            } else {
-                                level -= 1;
-                            }
+        let mut level = MAX_LEVEL - 1;
+        loop {
+            let x_guard = x.read().unwrap();
+            match &x_guard.nexts[level] {
+                Some(next) => {
+                    if &next.read().unwrap().value < val {
+                        let new_x = next.clone();
+                        drop(x_guard);
+                        x = new_x;
+                        continue;
+                    } else {
+                        prev[level] = Some(x.clone());
+                        if level == 0 {
+                            return (Some(next.clone()), prev);
+                        } else {
+                            level -= 1;
                         }
                     }
                 }
+                None => {
+                    prev[level] = Some(x.clone());
+                    if level == 0 {
+                        return (None, prev);
+                    } else {
+                        level -= 1;
+                    }
+                }
             }
-            None => (None, prev),
         }
     }
 }
@@ -158,9 +147,9 @@ mod test {
     fn insert_test() {
         let list = SkipList::new();
         for i in 0..1000 {
-            list.insert(i);   
+            list.insert(i);
         }
-        for (index, num) in list.iter().unwrap().enumerate() {
+        for (index, num) in list.iter().enumerate() {
             assert_eq!(num.read().unwrap().value, index);
         }
     }
@@ -175,7 +164,7 @@ mod test {
             list.insert(num);
         }
         nums.sort();
-        for (index, num) in list.iter().unwrap().enumerate() {
+        for (index, num) in list.iter().enumerate() {
             assert_eq!(num.read().unwrap().value, nums[index]);
         }
     }
@@ -187,7 +176,7 @@ mod test {
         let mut nums: Vec<Arc<Vec<u32>>> = Vec::new();
 
         const THREAD_NUM: usize = 8;
-        const NUMS_PER_THREAD: usize = 1000;
+        const NUMS_PER_THREAD: usize = 5;
         for _ in 0..THREAD_NUM {
             let mut local_nums = Vec::new();
             for _ in 0..NUMS_PER_THREAD {
@@ -214,7 +203,7 @@ mod test {
             t.join().unwrap();
         }
 
-        for (index, num) in list.iter().unwrap().enumerate() {
+        for (index, num) in list.iter().enumerate() {
             assert_eq!(num.read().unwrap().value, num_list[index]);
         }
     }
